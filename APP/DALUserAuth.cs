3890a713ea097c22de332827d58d23693dcbf9a5
iraheeel <iraheeel@gmail.com>
2023-10-19 06:16:17 +0500

    tasks 2

---
+++ APP/DALUserAuth.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+namespace DAL
+{
+    public class DALUserAuth
+    {
+
+        public static async Task<string> Authenticate(string username, string password)
+        {
+            UserSession userProfile = new UserSession();
+            try
+            {
+                using (SqlConnection con = DBHelper.GetConnection())
+                {
+                    await con.OpenAsync();
+                    using (SqlCommand cmd = new SqlCommand("LoginKaro", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@email", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+                        using (SqlDataReader sdr = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await sdr.ReadAsync())
+                            {
+                                // Ensure the name matches from your database variable storing name
+                                userProfile.name = sdr["name"].ToString();
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return userProfile.name.ToString();
+        }
+
+    }
+}
+
+
+
+
---
+++ APP/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBHelper
+    {
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=REARMS\\SQLEXPRESS;Initial Catalog=PracticeLab;Integrated Security=True");
+            return connection;
+        }
+
+    }
+}
